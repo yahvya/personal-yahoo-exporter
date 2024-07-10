7579ecd127afa1b16d51d679378269bc2a656b0b
Bathily Yahaya <developpeur.yb@gmail.com>
2024-07-10 14:30:14 +0200

    ajout des descriptions package.json ajout icone application (v1 fin - fichiers joints - recap .à ajouter)

--- electron/main.ts
+++ electron/main.ts
@@ -28,7 +28,7 @@ let win: BrowserWindow | null
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    icon: path.join(process.env.VITE_PUBLIC, 'app.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.mjs'),
     },
